b66c6d7a6c57a8f105a6590b5159641112b19cc7
Alex Munoz <luismunoz@Luiss-MacBook-Air.local>
2020-02-12 21:38:20 -0600

    makes import

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Button, Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core'
+import { Container } from '@material-ui/core/Container'
 
 const Import = (props) => {
     // fill out this component
+console.log(props)
 
     return (
-        <p>Import Component</p>
+        <div>
+        <Button onClick={props.fetchMakes} variant="contained" color="primary">Import</Button>
+    
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Id</TableCell>
+            <TableCell align="right">Make</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody> 
+          {props.makes.map(make => (
+            <TableRow key={make}>
+              <TableCell align="right">{make.MakeId}</TableCell>
+              <TableCell align="right">{make.MakeName}</TableCell>
+              {/* <TableCell align="right">{make.}</TableCell> */}
+            </TableRow>
+          ))}
+        </TableBody>
+
+        </div>
     )
 }
 
--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,17 @@
-// fill out this container
+import { connect } from 'react-redux'
+import Import from '../components/Import'
+import { fetchMakes } from '../redux/actions'
+
+const mapStateToProps = (state) => {
+    return {
+        makes: state.makes
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchMakes: (makes) => dispatch(fetchMakes(makes))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Import)
